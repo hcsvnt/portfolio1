9ba914cbba38941e0f173b6f65ce595d3dfd0d56
Hallala <mateuszhallala@gmail.com>
2021-05-06 00:36:19 +0200

    prefers color scheme works automatically, button toggle works as well, but the two don't work together yet

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -9,45 +9,69 @@ const useThemeDetection = () => {
         setIsDarkTheme(e.matches);
     });
 
-    useEffect(() => {
-        const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-        darkThemeMq.addEventListener(mediaQueryListener);
-        return () => darkThemeMq.removeEventListener(mediaQueryListener);
-    }, []);
+    // useEffect(() => {
+    //     const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+    //     darkThemeMq.addEventListener(mediaQueryListener);
+    //     return () => darkThemeMq.removeEventListener(mediaQueryListener);
+    // }, []);
+    return isDarkTheme;
 }
 
+const darkThemeValues = {
+    ['--bg-color']: '#1b1b1b',
+    ['--grid-clr']: 'hsl(0, 0%, 50%)',
+    color: 'white'
+}
 
+const lightThemeValues = {
+    ['--bg-color']: 'hsl(30, 50%, 94%)',
+    ['--grid-clr']: 'grey',
+    color: 'black'
+}
 
-
+const changeRootStyleValuesRapidly = (object) => {
+    const arrOfArr = Object.entries(object); 
+    // that's an array of arrays of key:value pairs as strings
+    arrOfArr.forEach(item => {
+        const [property, value] = item;
+        const rootElementStyle = document.documentElement.style;
+        rootElementStyle.setProperty(property, value);
+    }); 
+};
 
 
 const Layout = ({children}) => {
-    const getSystemTheme = () => window.matchMedia("(prefers-color-scheme: dark)").matches;
-    console.log(getSystemTheme())
 
+    const [theme, setTheme] = useState('light');
+    const isDarkTheme = useThemeDetection();
 
-    const changeRootStyleValuesRapidly = (object) => {
-        const arrOfArr = Object.entries(object); 
-        // that's an array of arrays of key:value pairs as strings
-        arrOfArr.forEach(item => {
-            const [property, value] = item;
-            const rootElementStyle = document.documentElement.style;
-            rootElementStyle.setProperty(property, value);
-        }); 
-    };
+    useEffect(() => {
+        if (isDarkTheme) {
+            setTheme('dark')
+            changeRootStyleValuesRapidly(darkThemeValues)
+        };
+    }, []);
 
-    const darkThemeValues = {
-        ['--bg-color']: '#1b1b1b',
-        ['--grid-clr']: 'hsl(0, 0%, 50%)',
-        color: 'white'
+    const themeToggle = () => {
+        console.log("let's get togglin'");
+       
+        if (theme === 'light') {
+            setTheme('dark');
+            changeRootStyleValuesRapidly(darkThemeValues)
+        }
+
+        if (theme === 'dark') {
+            setTheme('light');
+            changeRootStyleValuesRapidly(lightThemeValues)
+        }
     }
-
-    changeRootStyleValuesRapidly(darkThemeValues);
 
     return (
         <div className="wrapper">
             {children}
-            <button className="themeToggle">toggle mate</button>
+            <button className="themeToggle"
+                    onClick={themeToggle}
+            >toggle mate</button>
         </div>
     )
 }
