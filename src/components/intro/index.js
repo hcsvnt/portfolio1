50688c5beae99ef32bfcc14df23250fa445d53be
Hallala <mateuszhallala@gmail.com>
2021-08-10 10:11:46 +0200

    fixed matthallala link

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -28,7 +28,7 @@ const longText =    <article className={`${styles.introductionText} ${styles.tex
                         </p>
                         <p className={styles.introduction_p}>
                             Outside of coding I had been doing some freelance photography  
-                            (<a href="www.matthallala.com" target="blank">
+                            (<a href="https://www.matthallala.com/" target="blank">
                                 matthallala.com
                             </a>)
                             before covid struck, some documentary.
